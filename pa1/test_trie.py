8bdac486c438596c6a4409022c0472dd2a0702d8
Shrabya Timsina <shrabya@cs.uchicago.edu>
2017-01-09 17:47:02 -0600

    first try

--- pa1/test_trie.py
+++ pa1/test_trie.py
@@ -4,7 +4,7 @@
 #
 # YOUR NAME HERE
 
-from trie_dict import create_trie_node, add_word, is_word, num_completions, get_completions
+from trie_dict import create_trie_node, add_word, is_word, num_completions, get_completions, get_to_prefix_node
 
 t = create_trie_node()
 add_word("a", t)
@@ -19,3 +19,14 @@ add_word("bee", t)
 # correctly in the trie, the following statement will
 # cause the program to exit with an error message.
 assert is_word("bee", t)
+
+"""
+print(num_completions("a", t))
+print(num_completions("an", t))
+print(num_completions("and", t))
+print(num_completions("are", t))
+print(num_completions("bee", t))
+"""
+
+prefix_dict = get_to_prefix_node("z", t)
+print(prefix_dict)
--- pa1/trie_dict.py
+++ pa1/trie_dict.py
@@ -16,25 +16,78 @@ import trie_shell
 
 def create_trie_node():
     # replace None with an appropriate value
-    return None
+    return {'count': 0, 'final' : False }
 
 def add_word(word, trie):
     # replace pass with your code
-    pass
+    
+    trie['count'] += 1
+
+    if word[0] not in trie: 
+        trie[word[0]] = create_trie_node()
+
+    if len(word) == 1:
+        trie[word]['count'] = 1
+        trie[word]['final'] = True
+        return trie
+    
+    return add_word(word[1:], trie[word[0]])
+
+    
 
 def is_word(word, trie):
     # replace False with an appropriate value
-    return False
+    
+    if len(word) == 1:
+        return trie[word]['final']
+
+    return is_word(word[1:], trie[word[0]])
 
 def num_completions(word, trie):
     # replace 0 with an appropriate value
-    return 0
+
+    if len(word) == 1:
+        return trie[word]['count']
+
+    return num_completions(word[1:], trie[word[0]])
+
 
 def get_completions(word, trie):
     # replace [] with an appropriate value
-    return []
+    
+   # completion_list = []
+
+    
+
+    if trie[word]['count'] == 1 and  trie[word]['final'] == True:
+        completion_list.append(word)
+
+    
+
+def get_to_prefix_node(word, trie):
+    
+    if word[0] not in trie:
+        return None #if prefix not in dict or misspelled
+    
+    if len(word) == 1:
+        return trie[word]
 
 
+    return get_to_prefix_node(word[1:], trie[word[0]])
+
+def complete_prefix(prefix):
+
+    letters_only = {letter: prefix[letter] for k in ('l', 'm', 'n')}
+    
+    for letter in prefix.keys():
+
+
+
+
+
+
+"""
 if __name__ == "__main__":
     trie_shell.go("trie_dict")
 
+"""
