0329b5700f736ef1e251b5da1a2c7fc8d51a5a69
Shrabya Timsina <shrabya@cs.uchicago.edu>
2017-01-10 22:42:32 -0600

    corner cases left

--- pa1/trie_dict.py
+++ pa1/trie_dict.py
@@ -26,6 +26,7 @@ def add_word(word, trie):
     if word[0] not in trie: 
         trie[word[0]] = create_trie_node()
 
+    
     if len(word) == 1:
         trie[word]['count'] = 1
         trie[word]['final'] = True
@@ -38,46 +39,58 @@ def add_word(word, trie):
 def is_word(word, trie):
     # replace False with an appropriate value
     
-    if len(word) == 1:
-        return trie[word]['final']
+    if not word:
+        return False
 
-    return is_word(word[1:], trie[word[0]])
+    elif word[0] not in trie:
+        return False
+
+    else: 
+
+        if len(word) == 1:
+            return trie[word]['final']
+
+        return is_word(word[1:], trie[word[0]])
 
 def num_completions(word, trie):
     # replace 0 with an appropriate value
+    if not word:
+        return trie['count']
 
+    elif word[0] not in trie:
+        return 0
+   
     if len(word) == 1:
+
         return trie[word]['count']
 
     return num_completions(word[1:], trie[word[0]])
 
-
 def get_completions(word, trie):
     # replace [] with an appropriate value
-    
-   # completion_list = []
+    prefix_trie = get_to_prefix_node(word, trie)
 
-    
+    return complete_prefix(prefix_trie)
 
-    if trie[word]['count'] == 1 and  trie[word]['final'] == True:
-        completion_list.append(word)
-
+  
     
 
 def get_to_prefix_node(word, trie):
     
+    if not word: #check for empty prefix
+        return trie
+
     if word[0] not in trie:
         return None #if prefix not in dict or misspelled
     
-    if len(word) == 1:
-        return trie[word]
+    else:
+        if len(word) == 1:
+            return trie[word]
 
-
-    return get_to_prefix_node(word[1:], trie[word[0]])
+        return get_to_prefix_node(word[1:], trie[word[0]])
 
 def complete_prefix(trie):
-
-    
+   
 
 
     if trie['final'] == True and trie['count'] == 1:
@@ -85,21 +98,35 @@ def complete_prefix(trie):
 
     else:
         completions_list = []
-        
-        
+
+                
         exclude_keys = ['count', 'final']
         letters = [key for key in trie.keys() if key not in exclude_keys]
-
+        
+        if trie['count'] > 1 and trie['final'] == True:
+            completions_list.append("") 
+        
         for letter in letters:
-            print("letter: ", letter)
-            suffix = ""
-            suffix = suffix + letter + complete_prefix(trie[letter])[0]
-            print("suffix: ", suffix)
-            completions_list.append(suffix)
-            print("list: ", completions_list)
+            
+            #if trie[letter]['count'] > 1 and trie[letter]['final'] == True:
+                #completions_list.append(letter)
+
+            for leaf in complete_prefix(trie[letter]):
+
+                completions_list.append(letter + leaf)
+
+
+                #print("letter: ", letter)
+                
+                #suffix = suffix + letter + complete_prefix(trie[letter])[0]
+                #print("suffix: ", suffix)
+                #completions_list.append(suffix)
+                #print("list: ", completions_list)
+                #print("---------------------------")
+                 
 
     
-    return completions_list
+        return completions_list
 
 
 
@@ -118,8 +145,7 @@ def complete_prefix(trie):
 
 
 
-"""
+
 if __name__ == "__main__":
     trie_shell.go("trie_dict")
 
-"""
