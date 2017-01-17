229497952ae6a7c520b601bb22373e331372e70b
Shrabya Timsina <shrabya@cs.uchicago.edu>
2017-01-17 15:54:18 -0600

    indexer work

--- pa2/crawler.py
+++ pa2/crawler.py
@@ -40,9 +40,6 @@ reduce the soup to blocks of texts of classes
 ### YOUR FUNCTIONS HERE
 
 
-
-
-
 def convert_to_soup(url):
 
     request = util.get_request(url)
@@ -52,112 +49,77 @@ def convert_to_soup(url):
     soup = bs4.BeautifulSoup(html, 'lxml')
     return soup
 
-def build_dict(url):
+def build_dict(url, course_map_filename = 'course_map.json'):
     soup = convert_to_soup(url)
     course_list = soup.find_all('div', class_='courseblock main')
     index_dictionary  = {}
-    course_code_map = open_json_key()
+    course_code_map = open_json_key(course_map_filename)
 
     for course_block in course_list:
         
         course_block_title = course_block.find('p', class_='courseblocktitle')
         course_description = course_block.find('p', class_='courseblockdesc')
 
+        words_in_title = get_words_from_title(course_block_title.text)
+        if course_description is None:
+            words_in_description = []
+        else:
+            words_in_description = get_words_from_text(course_description.text)
+
         sequence = util.find_sequence(course_block)
 
-        #if there are no subsequences
         if not sequence:
-            #children = course_block.contents
-            #course_code_and_title = children[1].text
 
-            course_code = get_course_code(course_block_title.text)
-            course_identifier = course_code_map[course_code]
-
-            words_in_title = get_words_from_title(course_block_title.text)
-            words_in_description = get_words_from_description(course_description.text)
+            course_identifier = get_course_identifier(course_block_title.text, course_code_map)          
             
-            all_words = list(set(words_in_title + words_in_description))
-            words_to_index = [word for word in all_words if word not in INDEX_IGNORE]    
-            index_dictionary[course_identifier] = words_to_index
+            all_words = set(words_in_title + words_in_description)
+            words_to_index = all_words - INDEX_IGNORE
+            index_dictionary[course_identifier] = list(words_to_index)
 
         else:
-            #seq_codes_and_title = course_block.find('p', class_='courseblocktitle')
             
-            #course_seq_codes = get_course_code(course_block_title.text)
-            #course_code_list = course_seq_codes.split()
-            #course_code_list_proper = [course_code_list[0] + ' ' + code for code in course_code_list[1:]]
-            
-            words_in_seq_title = get_words_from_title(course_block_title.text)
-            words_in_seq_description = get_words_from_description(course_description.text)
-
-
             for subsequence in sequence:
                 
                 subsequence_title = subsequence.find('p', class_='courseblocktitle')
                 subsequence_description = subsequence.find('p', class_='courseblockdesc')
-                
-                subsequence_code = get_course_code(subsequence_title.text)
-                print(subsequence_code)
-                subsequence_identifier = course_code_map[subsequence_code]
+                              
+                subsequence_identifier = get_course_identifier(subsequence_title.text, course_code_map)
 
                 words_in_subseq_title = get_words_from_title(subsequence_title.text)
-                
-                if subsequence_description is not None:
-                    words_in_subseq_description = get_words_from_description(subsequence_description.text)
-                else:
+                if subsequence_description is None:
                     words_in_subseq_description = []
-
-                all_words = list(set(words_in_seq_title + words_in_seq_description + words_in_subseq_title + words_in_subseq_description))
-                words_to_index = [word for word in all_words if word not in INDEX_IGNORE]    
-                index_dictionary[subsequence_identifier] = words_to_index    
-
-
-
-                
-
-
-            
-
-
+                else:
+                    words_in_subseq_description = get_words_from_text(subsequence_description.text)
+                    
+                all_words = set(words_in_title + words_in_description 
+                                    + words_in_subseq_title + words_in_subseq_description)
+                words_to_index = all_words - INDEX_IGNORE
+                index_dictionary[subsequence_identifier] = list(words_to_index)
 
     return index_dictionary
 
-def get_course_code(course_code_and_title):
-    #get course identifier from course code
-    #course_code_pattern = r'[A-Z]{4}\s[0-9]+'
-    
-    #course_code = re.search(course_code_pattern, course_code_and_title).group()
-    #course_code = re.sub('[^A-Za-z0-9]+', ' ', course_code) #remove special character
-    #
-
+def get_course_identifier(course_code_and_title, course_code_map):
     course_code = re.match('.+[0-9]+\.', course_code_and_title).group()
     course_code = re.sub('[^A-Za-z0-9/s]+', ' ', course_code)
     course_code = course_code[:-1] #remove space at end
+    course_identifier = course_code_map[course_code]
+    return course_identifier
 
-    return course_code     
 
+def get_words_from_text(text_block):
+    text_block = text_block.lower()
+    text_block = re.sub('[^a-z0-9]+', ' ', text_block) 
+    words_pattern = r'[a-z][a-z0-9]*'
+    words = re.findall(words_pattern, text_block)
+    return words
 
 def get_words_from_title(course_code_and_title):
-    #extract words from title
-    course_title = course_code_and_title.lower()
-    course_title = re.sub('[^A-Za-z0-9]+', ' ', course_title)
-    words_pattern = r'[A-Za-z][A-Za-z0-9*]+'
-    words_in_title = re.findall(words_pattern, course_title)
+    words_in_title = get_words_from_text(course_code_and_title)
     words_in_title = words_in_title[1:] #removing first word which is part of course code
-    
     return words_in_title
 
-def get_words_from_description(description):
-    words_pattern = r'[A-Za-z][A-Za-z0-9*]+'
-    description = description.lower()
-    description = re.sub('[^A-Za-z0-9]+', ' ', description) 
-    words_in_description = re.findall(words_pattern, description)
-
-    return words_in_description
-
-
-def open_json_key():
-    with open('course_map.json') as json_data:
+def open_json_key(course_map_filename):
+    with open(course_map_filename) as json_data:
         course_number_data = json.load(json_data)
     return course_number_data
 
